d9f7b492662c6b79ca8a8a21a228aaf0829d2032
victorfmsilva <victormatoscps@gmail.com>
2020-04-14 13:19:38 -0300

    Atributos virtuais e Accessors

---
+++ Aula 10/classes.rb
@@ -0,0 +1,24 @@
+class Carro
+    attr_accessor :marca, :modelo  #O attr_accessor é um "atalho" que cria métodos de leitura, escrita e variável de instância em uma classe.
+    #attr_reader :marca, :modelo | metodo apenas para leitura caso façamos um set ele não funciona.
+    #attr_writer :marca, :modelo | metodo usado apenas para escrita
+
+    def velocidade_maxima
+        250
+    end
+
+    def descricao
+        "Marca: #{@marca} e Modelo: #{@modelo}" #interpolação da variavel
+    end
+
+end
+
+carro = Carro.new
+carro.marca = "Ford"
+carro.modelo = "Focus"
+puts "Marca: " + carro.marca
+puts "Modelo: " + carro.modelo
+puts "Descrição " + carro.descricao
+
+
+# ex: https://pt.stackoverflow.com/questions/63680/o-que-%C3%A9-o-attr-accessor-no-ruby/63704
